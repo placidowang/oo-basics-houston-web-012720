7aff87c67b5ebcfb95890c1d5bc1fb228ef19ccf
placidowang <placidow@gmail.com>
2020-01-28 13:52:09 -0600

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+require 'pry'
+
+class Book
+    def initialize (title)
+        @title = title
+    end
+
+    attr_accessor :title, :author, :page_count, :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
+# binding.pry
+puts "this is the end"
